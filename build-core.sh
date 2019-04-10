056339f251b83875dafae50777163920eee76be8
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2019-04-10 12:21:12 +0200

    Add support for Jetson nano

--- build-core.sh
+++ build-core.sh
@@ -16,7 +16,7 @@
 
 print_usage()
 {
-    printf "Usage: %s <tx1|tx2> <authority_id> <gadget_snap> <kernel_snap> [key_id]\n" \
+    printf "Usage: %s <tx1|tx2|nano> <authority_id> <gadget_snap> <kernel_snap> [key_id]\n" \
            "$(basename "$0")"
 }
 
@@ -26,7 +26,7 @@ if [ $# -lt 4 ] || [ $# -gt 5 ]; then
     exit 1
 fi
 board=$1
-if [ "$board" != tx1 ] && [ "$board" != tx2 ]; then
+if [ "$board" != tx1 ] && [ "$board" != tx2 ] && [ "$board" != nano ]; then
     print_usage
     exit 1
 fi
@@ -70,20 +70,37 @@ ubuntu-image snap --output-dir "$outdir" --workdir "$outdir" \
 
 # Generate tarball with all the needed parts for flashing
 final_tree="$outdir"/final_tree
-bootloader_dir="$final_tree"/bootloader
-if [ "$board" = tx1 ]; then
-    u_boot_dir="$bootloader_dir"/t210ref/p2371-2180
-    writable_part=18
+mkdir "$final_tree"
+if [ "$board" = nano ]; then
+    pushd "$outdir"
+    wget https://developer.nvidia.com/embedded/dlc/l4t-jetson-driver-package-32-1-jetson-nano \
+         -O Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
+    tar xf Jetson-Nano-Tegra210_Linux_R32.1.0_aarch64.tbz2
+    cp unpack/gadget/u-boot.bin Linux_for_Tegra/bootloader/t210ref/p3450-porg/
+    cp volumes/jetson/part0.img Linux_for_Tegra/bootloader/system-boot.ext4
+    cp volumes/jetson/part12.img Linux_for_Tegra/bootloader/system-data.ext4
+    cd Linux_for_Tegra
+    patch -p1 < ../../tarball-parts/nano/create-jetson-nano-sd-card-image.sh.patch
+    sudo ./create-jetson-nano-sd-card-image.sh -o jetson.img -s 600M -r 200
+    popd
+    mv "$outdir"/Linux_for_Tegra/jetson.img "$final_tree"
+    cp tarball-parts/"$board"/README.md "$final_tree"
 else
-    u_boot_dir="$bootloader_dir"/t186ref/p2771-0000/500
-    writable_part=30
+    bootloader_dir="$final_tree"/bootloader
+    if [ "$board" = tx1 ]; then
+        u_boot_dir="$bootloader_dir"/t210ref/p2371-2180
+        writable_part=18
+    else
+        u_boot_dir="$bootloader_dir"/t186ref/p2771-0000/500
+        writable_part=30
+    fi
+    mkdir -p "$u_boot_dir"
+    cp "$outdir"/unpack/gadget/u-boot.bin "$u_boot_dir"
+    cp "$outdir"/volumes/jetson/part0.img "$bootloader_dir"/system.img
+    cp "$outdir"/volumes/jetson/part"$writable_part".img "$bootloader_dir"/system-data.ext4
+    cp tarball-parts/"$board"/* "$final_tree"
 fi
-mkdir -p "$u_boot_dir"
-cp "$outdir"/unpack/gadget/u-boot.bin "$u_boot_dir"
-cp "$outdir"/volumes/jetson/part0.img "$bootloader_dir"/system.img
-cp "$outdir"/volumes/jetson/part"$writable_part".img "$bootloader_dir"/system-data.ext4
-cp tarball-parts/"$board"/* "$final_tree"
-pushd .
-cd "$final_tree"
+
+pushd "$final_tree"
 tar -cJf ../core-18-jetson-"$board".tar.xz -- *
 popd
---
+++ tarball-parts/nano/README.md
@@ -0,0 +1,10 @@
+# Intructions for flashing Ubuntu Core 18 in Jetson TX2
+
+You simply need to copy into the SD card from your computer:
+
+```
+sudo sh -c 'cat jetson.img > /dev/<SD_card_device>'
+sync
+```
+
+Then, insert the SD card into the device and boot. Done!
